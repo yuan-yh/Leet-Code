0273ee24ed8010d4e5281aef96b8a98f3b87166d
Yuan-ueng <yuanyueheng2019@163.com>
2025-03-19 08:32:34 -0700

    group anagram

---
+++ 49_Group Anagrams.java
@@ -0,0 +1,20 @@
+// Time complexity: O(m∗n)
+// Space complexity: O(m)
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            // 1. count letters in s as key
+            int[] count = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+            }
+            // 2. add into the HashMap
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
