e69cfbcaac92b791d7d58fe05ef770d9f8114634
Yuan-ueng <yuanyueheng2019@163.com>
2025-04-23 14:21:46 -0700

    meta 680

--- 680_Valid Palindrome II.java
+++ 680_Valid Palindrome II.java
@@ -1,3 +1,24 @@
 // Two Pointers (Optimal)
 // Time complexity: O(n)
 // Space complexity: O(1)
+
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return (isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1));
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
