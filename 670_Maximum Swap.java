747ef404fbde15d4045b2b3ccf1df6ea6144fdb0
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-05-07 18:47:19 -0700

    tiktok 670 TBC

---
+++ 670_Maximum Swap.java
@@ -0,0 +1,11 @@
+// Space Optimized Greedy
+// Time complexity: O(n)
+// Spaace complexity: O(n)
+
+class Solution {
+    public int maximumSwap(int num) {
+        int res = num;
+        // loop leftwards to find the max digit at the right side of the current digit
+        return res;
+    }
+}
