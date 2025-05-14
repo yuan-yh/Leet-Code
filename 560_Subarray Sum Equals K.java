75573264f9b6f77082105ff7fc1258b408b5a497
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-05-14 15:10:52 -0700

    meta 560

--- 560_Subarray Sum Equals K.java
+++ 560_Subarray Sum Equals K.java
@@ -1,19 +1,20 @@
 // Time complexity : O(n)
 // Space complexity : O(n)
 
+// prefix sum: psum[0:i) + sum[i:j] = psum[0:j]
+// Therefore, if sum[i:j] = k -> psum[0:j] - psum[0:i) = k
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0, sum = 0;
-        // <sum, count of subarray with this sum>
-        Map<Integer, Integer> m = new HashMap<>();
-        // init：前缀和为 0 出现 1 次（空子数组）
+        Map<Integer, Integer> m = new HashMap<>();  // <psum, count>
         m.put(0, 1);
+
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            // 检查是否存在前缀和等于 sum - k
             if (m.containsKey(sum - k)) count += m.get(sum - k);
             m.put(sum, m.getOrDefault(sum, 0) + 1);
         }
+
         return count;
     }
 }
