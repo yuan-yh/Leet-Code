d41ed887a90725afe299406f71130f9c52b284ae
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-05-21 17:44:03 -0700

    灵茶山 06

--- 24_Swap Nodes in Pairs.java
+++ 24_Swap Nodes in Pairs.java
@@ -11,6 +11,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Method 1: iteration
 class Solution {
     public ListNode swapPairs(ListNode head) {
         // edge case: null or one node only
@@ -33,5 +35,25 @@ class Solution {
         }
 
         return vHead.next;
+    }
+}
+
+// Method 2: recursion
+// 时间复杂度：O(n)，其中 n 为链表长度。
+// 空间复杂度：O(n)
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode node1 = head;
+        ListNode node2 = head.next;
+        ListNode node3 = node2.next;
+
+        node1.next = swapPairs(node3); // 1 指向递归返回的链表头
+        node2.next = node1; // 2 指向 1
+
+        return node2; // 返回交换后的链表头节点
     }
 }
