e5272ef0d8a2cfb94b679d6ffd0f9d24e2f3a240
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-06-13 16:33:31 -0700

    灵茶山 12

--- 235_Lowest Common Ancestor of a Binary Search Tree.java
+++ 235_Lowest Common Ancestor of a Binary Search Tree.java
@@ -1,7 +1,7 @@
 // https://www.bilibili.com/video/BV1W44y1Z7AR/?spm_id_from=333.337.search-card.all.click&vd_source=3a9127d6a2a8aebf3636c857ed27ccfa
 // DFS
-// Time Complexity: O(N)
-// Space Complexity: O(N)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
 /**
  * Definition for a binary tree node.
@@ -15,8 +15,7 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode node, TreeNode p, TreeNode q) {
-        if (node == null || node == p || node == q) return node;
-
+        // No need to verify null node, as the problem ensures that the LCA exists
         // 3 cases based on the BST: both < - left tree; both > - right tree; in-between - current node is LCA
         if (p.val < node.val && q.val < node.val) return lowestCommonAncestor(node.left, p, q);
         if (p.val > node.val && q.val > node.val) return lowestCommonAncestor(node.right, p, q);
--- 236_Lowest Common Ancestor of a Binary Tree.java
+++ 236_Lowest Common Ancestor of a Binary Tree.java
@@ -1,7 +1,7 @@
 // https://www.bilibili.com/video/BV1W44y1Z7AR/?spm_id_from=333.337.search-card.all.click&vd_source=3a9127d6a2a8aebf3636c857ed27ccfa
 // DFS
-// Time Complexity: O(N)
-// Space Complexity: O(N)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
 /**
  * Definition for a binary tree node.
