b846b08d41572ab23c5c5027a2f2c4803e099364
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-06-18 12:17:56 -0700

    灵茶山 15

--- 77_Combinations.java
+++ 77_Combinations.java
@@ -1,3 +1,30 @@
-// Time Complexity: O(n^2)
+// Time Complexity: O(k * C(n,k))
 // Space Complexity: O(n)
 
+class Solution {
+    private List<List<Integer>> res = new ArrayList<>();
+    private List<Integer> cur = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        bt(1, n, k); 
+        return res;
+    }
+
+    private void bt(int start, int end, int numsLeft) {
+        // end case
+        if (numsLeft == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        // cut branch
+        if (end - start + 1 < numsLeft) return;
+
+        for (int i = start; i <= end; i++) {
+            // process
+            cur.add(i);
+            bt(i+1, end, numsLeft-1);
+            // backtrack
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
