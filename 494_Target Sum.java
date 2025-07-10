df7dc55f14e3477b920b16b2262f3d6232aa0bb6
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-07-10 13:44:03 -0700

    灵茶山18

--- 494_Target Sum.java
+++ 494_Target Sum.java
@@ -1,25 +1,29 @@
 class Solution {
-    private int[] nums;
-
     public int findTargetSumWays(int[] nums, int target) {
         // Method 1 (slow): binary tree - each branch is eith + or -
         // Method 2: 0-1背包 (DP)
-        // sum of selected positive nums[i] = p
-        // the absolute value for sum of selected negative nums[i] = q
+        // sum of all selected positive nums[i] = p
+        // the absolute value for sum of all selected negative nums[i] = q
         // p + q = complete array sum s
         // p - q = target t
-        // Therefore, 
-        // for target > 0 so p > q
-        //  q = (s - t) / 2 => pack capacity = q as space optimization => how many ways to fill the backpack
-        // OR for target < 0 so p < q
-        //  p = (s + t) / 2 => pack capacity = p as space optimization => how many ways to fill the backpack
-        // OVERALL: for backpack size optimization -> (s - abs(t)) / 2
-        int s = 0;
-        for (int n : nums) s += n;
-        s -= Math.abs(target);
-        // optimization
-        if (s < 0 || s%2 == 1) return 0;
-        s /= 2;     // backpack size
-        this.nums = nums;
+        // Therefore, p = (t + s) / 2 => bag size = p => how many ways to fill the backpack
+
+        int sum = 0;
+        for (int n : nums) sum += n;
+        // cut branch
+        if ((sum + target) % 2 != 0 || (sum + target) < 0) return 0;
+
+        // 0-1背包问题
+        int bagSize = (sum + target) / 2;
+        int[] dp = new int[bagSize + 1];
+        dp[0] = 1;
+
+        for (int i : nums) {
+            for (int j = bagSize; j >= i; j--) {
+                if (dp[j - i] > 0) dp[j] += dp[j-i];
+            }
+        }
+
+        return dp[bagSize];
     }
 }
