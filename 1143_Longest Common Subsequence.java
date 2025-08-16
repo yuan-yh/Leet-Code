0e8bfcac713195ddd52ba06585d24e33eded0a71
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-08-16 13:14:33 -0700

    dp

--- 1143_Longest Common Subsequence.java
+++ 1143_Longest Common Subsequence.java
@@ -1,34 +1,24 @@
 class Solution {
-    private int[][] record;
-    private String s1;
-    private String s2;
-
     public int longestCommonSubsequence(String text1, String text2) {
-        int l1 = text1.length(), l2 = text2.length();
-        this.s1 = text1;
-        this.s2 = text2;
-        this.record = new int[l1][l2];
+        int l1 = text1.length(), l2 = text2.length(), res = 0;
+        int[][] dp = new int[l1+1][l2+1];
 
-        // init
-        for (int i = 0; i < l1; i++) {
-            for (int j  = 0; j < l2; j++) {
-                record[i][j] = -1;
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if (dp[i][j-1] == i) {
+                    dp[i][j] = dp[i][j-1];
+                    continue;
+                } 
+
+
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    res = Math.max(res, dp[i][j]);
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-
-        return dfs(text1.length()-1, text2.length()-1);
-    }
-
-    private int dfs(int i, int j) {
-        // end case
-        if (i < 0 || j < 0) return 0;
-        if (record[i][j] != -1) return record[i][j];
-
-        // process
-        int res = 0;
-        if (s1.charAt(i) == s2.charAt(j)) res = (1+dfs(i-1, j-1));
-        else res = Math.max(dfs(i-1, j), dfs(i, j-1));
-        record[i][j] = res;
         return res;
     }
 }
