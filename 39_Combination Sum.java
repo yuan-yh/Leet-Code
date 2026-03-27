233c9b5dd14b3169feef6f326dceb953ef895fe1
Yuan-ueng <yuan.yueh@northeastern.edu>
2026-03-27 12:50:37 -0700

    update hot-100

--- 39_Combination Sum.java
+++ 39_Combination Sum.java
@@ -1,27 +1,33 @@
 class Solution {
     private List<List<Integer>> res = new ArrayList<>();
-    private List<Integer> cur = new ArrayList<>();
-    private int[] nums;
+    private List<Integer> curPath = new ArrayList<>();
+    private int[] candidates;
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        this.nums = candidates;
-        bt(0, target);
-        return res;
-    }
-
-    private void bt(int startIndex, int target) {
+    private void bt(int idx, int left) {
         // end case
-        if (target == 0) {
-            res.add(new ArrayList<>(cur));
+        if (left == 0) {
+            res.add(new ArrayList<>(curPath));
             return;
         }
-        // cut branch
-        if (target < 0) return;
-        // process & backtrack
-        for (int i = startIndex; i < nums.length; i++) {
-            cur.add(nums[i]);
-            bt(i, target - nums[i]);
-            cur.remove(cur.size() - 1);
+        // process
+        for (int i = idx; i < candidates.length; i++) {
+            // short-cut: terminate if beyond target
+            if (candidates[i] > left) break;
+            // short-cut: skip to avoid duplicates
+            if (i > idx && candidates[i] == candidates[idx]) continue;
+
+            curPath.add(candidates[i]);
+            bt(i, left - candidates[i]);
+            curPath.remove(curPath.size() - 1);
         }
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        this.candidates = candidates;
+        // 1. sort
+        Arrays.sort(candidates);
+        // 2. process
+        bt(0, target);
+        return this.res;
     }
 }
--- 39_Combination Sum.py
+++ 39_Combination Sum.py
@@ -1,23 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         # 完全背包: 每个物品可以用无限次
-        # At each c[i], you may not choose or choose from [i:]
-        res, curPath = [], []
-
-        def bt(start, curSum):
+        # Build a N-nary tree: either not choose or choose from [start:]
+        def bt(idx, curSum):
             # end case
             if curSum == target:
                 res.append(list(curPath))
                 return
             # process
-            for i in range(start, len(candidates)):
-                # cut branch
-                if curSum + candidates[i] > target: break
-
+            for i in range(idx, len(candidates)):
+                # short-cut: terminate when > target
+                if candidates[i] + curSum > target: break
+                # short-cut: skip to avoid duplicates
+                if i > idx and candidates[i] == candidates[idx]: continue
                 curPath.append(candidates[i])
                 bt(i, curSum + candidates[i])
                 curPath.pop()
         
+        # 1. sort
         candidates.sort()
+        # 2. bt
+        curPath, res = [], []
         bt(0, 0)
         return res
