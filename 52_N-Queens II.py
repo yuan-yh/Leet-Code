83a172fad56ae63f07340e0a57746f5fe3071020
Yuan-ueng <yuan.yueh@northeastern.edu>
2026-04-01 18:28:52 -0700

    update hot-100

--- 52_N-Queens II.py
+++ 52_N-Queens II.py
@@ -1,6 +1,6 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        def dfs(curRow: int):
+        def dfs(curRow):
             # end case
             if curRow == n:
                 nonlocal res
@@ -8,20 +8,16 @@ class Solution:
                 return
             # process
             for c in range(n):
-                if not(colb[c] or diagP[curRow + c] or diagN[curRow - c + n - 1]):
-                    colb[c] = diagP[curRow + c] = diagN[curRow - c + n - 1] = True
-                    board[curRow][c] = 'Q'
+                if not(colb[c] or diagP[curRow+c] or diagN[curRow-c+n-1]):
+                    colb[c] = diagP[curRow+c] = diagN[curRow-c+n-1] = True
                     dfs(curRow + 1)
-                    board[curRow][c] = '.'
-                    colb[c] = diagP[curRow + c] = diagN[curRow - c + n - 1] = False
-
+                    colb[c] = diagP[curRow+c] = diagN[curRow-c+n-1] = False
+        
         # 1. init visit records
-        board = [['.'] * n for _ in range(n)]
         colb = [False] * n
-        # For all grids along the same positive diagnoal, r+c - constant
-        # For all grids along the same negative diagnoal, r-c+(n-1) - constant
+        # For grids along the same positive diagonal, r+c - constant
+        # For grids along the same negative diagonal, r-c+(n-1) - constant
         diagP, diagN = [False] * (2*n - 1), [False] * (2*n - 1)
-
         # 2. dfs
         res = 0
         dfs(0)
