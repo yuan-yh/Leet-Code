bdc41f965cab598dd69b0540650a9313bbed2504
Yuan-ueng <yuan.yueh@northeastern.edu>
2026-04-04 12:46:44 -0700

    update hot-100

--- 155_Min Stack.py
+++ 155_Min Stack.py
@@ -1,18 +1,20 @@
 class MinStack:
+    # Keep tracking the valid min-val at the cur stack element
     def __init__(self):
-        self.minstack = [(0, inf)]  # init with (any_val, min)
+        self.minStack = [(0, inf)]  # init with (any_val, min)
 
     def push(self, val: int) -> None:
-        self.minstack.append((val, min(val, self.minstack[-1][1])))
+        self.minStack.append((val, min(val, self.minStack[-1][1])))
 
     def pop(self) -> None:
-        self.minstack.pop()
+        self.minStack.pop()
 
     def top(self) -> int:
-        return self.minstack[-1][0]
+        return self.minStack[-1][0]
 
     def getMin(self) -> int:
-        return self.minstack[-1][1]
+        return self.minStack[-1][1]
+        
 
 
 # Your MinStack object will be instantiated and called as such:
