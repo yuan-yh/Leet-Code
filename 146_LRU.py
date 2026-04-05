1cd3a6da9aa501a219bbc6e6cf0497cb241205db
Yuan-ueng <yuan.yueh@northeastern.edu>
2026-04-05 09:53:55 -0700

    update amz-list

--- 146_LRU.py
+++ 146_LRU.py
@@ -1,52 +1,62 @@
+
 class Node:
-    __slots__ = 'key', 'val', 'prev', 'next'
+    __slots__ = "key", "val", "prev", "next"
+
     def __init__(self, key=0, val=0):
         self.key = key
         self.val = val
-
+        self.prev = None
+        self.next = None
+    
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.record = {}            # key: node.key, val: node
+        self.record = {}
         self.head = Node()
         self.tail = Node()
+
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def delete(self, node):
-        pnode, nnode = node.prev, node.next
-        pnode.next = nnode
-        nnode.prev = pnode
+    def breakConnect(self, node):
+        pn, nn = node.prev, node.next
+        pn.next = nn
+        nn.prev = pn
     
-    def insert(self, node):
-        phead = self.head.next
+    def insertFront(self, node):
+        th = self.head.next
         self.head.next = node
         node.prev = self.head
-        node.next = phead
-        phead.prev = node
+        node.next = th
+        th.prev = node
     
     def get(self, key: int) -> int:
+        # 1. fetch node
         if key not in self.record: return -1
         target = self.record[key]
-        self.delete(target)
-        self.insert(target)
+        # 2. break connection
+        self.breakConnect(target)
+        # 3. insert front
+        self.insertFront(target)
         return target.val
 
     def put(self, key: int, value: int) -> None:
-        if key in self.record:
+        # 1. fetch existing node & update val
+        if key in self.record: 
             target = self.record[key]
             target.val = value
-            self.delete(target)
+            self.breakConnect(target)
+        # 2. create new node
         else: 
             target = Node(key, value)
             self.record[key] = target
-        
-        self.insert(target)
-
+        # 3. delete if above capacity
         if len(self.record) > self.capacity:
-            expire = self.tail.prev
-            self.delete(expire)
-            del self.record[expire.key]
+            toDel = self.tail.prev
+            self.breakConnect(toDel)
+            del self.record[toDel.key]
+        # 4. insert front
+        self.insertFront(target)
 
 
 # Your LRUCache object will be instantiated and called as such:
