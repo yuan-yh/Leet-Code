3817b5f349fd5496b0de30c2d3c157a0c982de27
Yuan-ueng <yuan.yueh@northeastern.edu>
2026-04-06 09:06:11 -0700

    update amz-list

--- 42_Trapping Rain Water.py
+++ 42_Trapping Rain Water.py
@@ -1,16 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         # 2-Pointer: shift towards center
-        res, l, r, lMax, rMax = 0, 0, len(height) - 1, height[0], height[-1]
+        # the lower boundary determines the trapped amount
+        res, l, r, lMax, rMax = 0, 0, len(height) - 1, 0, 0
 
-        while l < r:
-            lMax = max(lMax, height[l])
-            rMax = max(rMax, height[r])
-            # the lower boundary determines the trapped amount
+        while l <= r:
             if lMax < rMax:
+                lMax = max(lMax, height[l])
                 res += lMax - height[l]
                 l += 1
             else:
+                rMax = max(rMax, height[r])
                 res += rMax - height[r]
                 r -= 1
+
         return res
