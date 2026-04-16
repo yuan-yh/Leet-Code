0ede1c85eca1843652ab3ca84914ac239f77a143
Yuan-ueng <yuan.yueh@northeastern.edu>
2026-04-16 15:34:40 -0700

    amz list

--- 238_Product of Array Except Self.py
+++ 238_Product of Array Except Self.py
@@ -1,15 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = []
-
-        pre = post = 1
-        # 1. build pre-product
+        lp = rp = 1
+        # 1. build left-side product
         for n in nums:
-            res.append(pre)
-            pre *= n
-        # 2. build post-product
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= post
-            post *= nums[i]
-        
+            res.append(lp)
+            lp *= n
+        # 2. build right-side product
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= rp
+            rp *= nums[i]
         return res
