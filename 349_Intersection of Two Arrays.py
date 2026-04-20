6320cf6a5aa3f03fed6af08070016c15bf4deaee
Yuan-ueng <yuan.yueh@northeastern.edu>
2026-04-20 15:14:55 -0700

    amz list

--- 349_Intersection of Two Arrays.py
+++ 349_Intersection of Two Arrays.py
@@ -1,10 +1,17 @@
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        s1 = set(nums1)
+    # Method 1
+    def intersection(self, n1: List[int], n2: List[int]) -> List[int]:
+        n1 = set(n1)
         res = []
-        for n in nums2:
-            if n in s1:
-                s1.remove(n)
+        for n in set(n2):
+            if n in n1:
                 res.append(n)
-        
+                n1.remove(n)
+
         return res
+    
+    # # Method 2
+    # def intersection(self, n1: List[int], n2: List[int]) -> List[int]:
+    #     # hashset then compare
+    #     n1, n2 = set(n1), set(n2)
+    #     return list(n1 & n2)
--- 739_Daily Temperatures.py
+++ 739_Daily Temperatures.py
@@ -1,12 +1,33 @@
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
-        res = [0] * len(temperatures)
-
-        for i in range(len(temperatures)-1, -1, -1):
-            # case: t[stack[-1]] <= t[i] -> pop & loop stack
-            while stack and temperatures[stack[-1]] <= temperatures[i]: stack.pop()
-            # case: t[stack[-1]] > t[i] -> date = stack[-1] - i
-            if stack: res[i] = stack[-1] - i
-            stack.append(i)
+    # Method 1: left-to-right, both O(n)
+    def dailyTemperatures(self, temp: List[int]) -> List[int]:
+        res = [0] * len(temp)
+        q = []
+        for i in range(len(temp)):
+            # case: empty || temp[q[-1]] < cur || temp[q[-1]] >= cur
+            while q and temp[q[-1]] < temp[i]:
+                res[q[-1]] = i - q[-1]
+                q.pop()
+            q.append(i)
         return res
+    
+    # # Method 2: right-to-left
+    # def dailyTemperatures(self, temp: List[int]) -> List[int]:
+    #     res = []
+    #     q = deque()     # (idx)
+    #     for i in range(len(temp)-1, -1, -1):
+    #         # case: empty q: append
+    #         # case: cur_temp >= temp[q[-1]]: keep pop
+    #         #   if empty: append
+    #         #   if not: calculate idx then append
+    #         # case: cur_temp < temp[q[-1]]: calculate idx then append
+    #         while q and temp[q[-1]] <= temp[i]: q.pop()
+    #         res.append(0 if not q else q[-1] - i)
+    #         q.append(i)
+        
+    #     # reverse res
+    #     l, r = 0, len(res) - 1
+    #     while l < r:
+    #         res[l], res[r] = res[r], res[l]
+    #         l, r = l + 1, r - 1
+    #     return res
