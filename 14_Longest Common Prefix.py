20d2f37ddd36b5ae9baff1cd0efa4816d9bac67d
Yuan-ueng <yuan.yueh@northeastern.edu>
2026-05-29 15:38:13 -0700

    freq list

--- 14_Longest Common Prefix.py
+++ 14_Longest Common Prefix.py
@@ -1,6 +1,5 @@
 class Node:
     __slots__ = "children", "end"
-
     def __init__(self):
         self.children = {}
         self.end = False
@@ -9,7 +8,6 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         # 1. build Trie
         head = Node()
-
         for s in strs:
             cur = head
             for c in s:
@@ -17,17 +15,9 @@ class Solution:
                     cur.children[c] = Node()
                 cur = cur.children[c]
             cur.end = True
-        
         # 2. traverse
-        res = ""
         cur = head
-
-        while cur:
-            # check for end or only one children
-            if cur.end or len(cur.children) > 1: break
-
-            key = next(iter(cur.children))      # iter() 转成迭代器, next() 获取第一个元素
-            res += key
-            cur = cur.children[key]
-
-        return res
+        for i, c in enumerate(strs[0]):
+            if len(cur.children) > 1 or cur.end: return strs[0][:i]
+            cur = cur.children[c]
+        return strs[0]
